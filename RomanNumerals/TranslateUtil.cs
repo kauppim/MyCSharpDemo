8248f2c072239c2bcb9529f0e6cb4c6a37d911e6
Mikael Kauppinen <leakim@iki.fi>
2023-01-21 16:26:47 +0200

    Replace initial commit with sln structure

---
+++ RomanNumerals/TranslateUtil.cs
@@ -0,0 +1,12 @@
+﻿namespace RomanNumerals;
+public static class TranslateUtil
+{
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+}
